1aee1718ceb8907a6ea44fc10c6e68e4fcd549be
HannHannie <ahihihohohahahihi@gmail.com>
2025-03-06 21:43:25 +0700

    delete C

--- Ques04.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-int main(){
-    int x, y, cal;
-    scanf("%d %d", &x, &y);
-    cal= 3*pow(x,2)*pow(y,2)-2*x*pow(y,2)-7*pow(x,2)*y-4*pow(y,2)+15*x*y+2*pow(x,2)-3*x+10*y+6;
-    printf("Result is: %d", cal);
-}
